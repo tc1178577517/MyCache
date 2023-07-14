2a5433d7c2be48d7281429349e19cd8c5491b98c
Volcano <1178577517@qq.com>
2023-07-14 16:21:17 +0800

    淘汰策略——不淘汰，啥也不干

---
+++ cache/src/main/java/com/tc/support/evict/CacheEvictNone.java
@@ -0,0 +1,16 @@
+package com.tc.support.evict;
+
+import com.tc.api.ICacheEvict;
+import com.tc.api.ICacheEvictContext;
+
+/**
+ * 淘汰策略——无淘汰
+ * @param <K>
+ * @param <V>
+ */
+public class CacheEvictNone <K, V> implements ICacheEvict<K, V> {
+    @Override
+    public void evict(ICacheEvictContext<K, V> context) {
+
+    }
+}
