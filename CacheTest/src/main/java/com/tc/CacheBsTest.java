fc0419ba44f72c28732760b7a1fb1f63f333f2c9
Volcano <1178577517@qq.com>
2023-07-17 14:38:16 +0800

    增加过期测试

--- CacheTest/src/main/java/com/tc/CacheBsTest.java
+++ CacheTest/src/main/java/com/tc/CacheBsTest.java
@@ -6,13 +6,13 @@ import com.tc.core.Cache;
 import org.junit.Assert;
 import org.junit.Test;
 
-
+import java.util.concurrent.TimeUnit;
 
 
 public class CacheBsTest {
 
     @Test
-    public void helloTest(){
+    public void release0_1Test(){
         Cache<String, String> cache = (Cache<String, String>) CacheBootstrap.<String, String>newInstance()
                 .size(3)
                 .build();
@@ -23,6 +23,23 @@ public class CacheBsTest {
         cache.put("4", "4");
 
         Assert.assertEquals(3, cache.size());
+        System.out.println(cache.keySet());
+    }
+
+    @Test
+    public void release0_2Test() throws InterruptedException {
+        Cache<String, String> cache = (Cache<String, String>) CacheBootstrap.<String,String>newInstance()
+                .size(3)
+                .build();
+        cache.put("1", "1");
+        cache.put("2", "2");
+
+        cache.expire("1", 10);
+        Assert.assertEquals(2, cache.size());
+
+        TimeUnit.MILLISECONDS.sleep(50);
+        Assert.assertEquals(1, cache.size());
+
         System.out.println(cache.keySet());
     }
 }
