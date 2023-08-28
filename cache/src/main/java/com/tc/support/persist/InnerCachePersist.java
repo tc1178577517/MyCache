3da0eacaf326ca4944df87bc3071248c2d78d9be
tc1178577517 <92702329+tc1178577517@users.noreply.github.com>
2023-08-28 16:20:36 +0800

    内置的线程定时持久化（Json形式）

---
+++ cache/src/main/java/com/tc/support/persist/InnerCachePersist.java
@@ -0,0 +1,56 @@
+package com.tc.support.persist;
+
+
+import com.tc.api.ICache;
+import com.tc.api.ICachePersist;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 定时执行的持久化策略
+ * @param <K>
+ * @param <V>
+ */
+public class InnerCachePersist <K, V>{
+    /**
+     * 缓存信息
+     */
+    private final ICache<K,V> cache;
+
+    /**
+     * 缓存持久化策略
+     */
+    private final ICachePersist<K,V> persist;
+
+    /**
+     * 线程执行类
+     * @since 0.0.3
+     */
+    private static final ScheduledExecutorService EXECUTOR_SERVICE = Executors.newSingleThreadScheduledExecutor();
+
+    public InnerCachePersist(ICache<K, V> cache, ICachePersist<K, V> persist) {
+        this.cache = cache;
+        this.persist = persist;
+        
+        this.init();
+    }
+
+    private void init() {
+        EXECUTOR_SERVICE.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    System.out.println("开始以Json格式持久化缓存");
+                    persist.persist(cache);
+                    System.out.println("持久化缓存结束");
+                }catch (Exception e){
+                    System.out.println("持久化异常");
+                    e.printStackTrace();
+                }
+
+            }
+        }, 0, 10, TimeUnit.MINUTES);
+    }
+}
