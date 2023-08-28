db671a76ce49919698d580b7935c58e8881162cf
tc1178577517 <92702329+tc1178577517@users.noreply.github.com>
2023-08-28 16:22:30 +0800

    缓存持久化工具类

---
+++ cache/src/main/java/com/tc/support/persist/CachePersists.java
@@ -0,0 +1,39 @@
+package com.tc.support.persist;
+
+import com.tc.api.ICachePersist;
+
+/**
+ * 缓存持久化工具类
+ * @author binbin.hou
+ * @since 0.0.8
+ */
+public final class CachePersists {
+
+    private CachePersists() {
+    }
+
+    /**
+     * 无操作
+     *
+     * @param <K> key
+     * @param <V> value
+     * @return 结果
+     * @since 0.0.8
+     */
+    public static <K, V> ICachePersist<K, V> none() {
+        return  new CachePersistNone<K, V>();
+    }
+
+    /**
+     * 无操作
+     *
+     * @param <K>  key
+     * @param <V>  value
+     * @param path 文件路径
+     * @return 结果
+     * @since 0.0.8
+     */
+    public static <K, V> ICachePersist<K, V> dbJson(final String path) {
+        return new CachePersistDbJson<>(path);
+    }
+}
