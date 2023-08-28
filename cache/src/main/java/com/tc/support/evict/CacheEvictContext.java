af54493b9af5e7b2785ea2bf90d5780e130b3f4c
tc1178577517 <92702329+tc1178577517@users.noreply.github.com>
2023-08-28 16:24:54 +0800

    淘汰上下文更新

--- cache/src/main/java/com/tc/support/evict/CacheEvictContext.java
+++ cache/src/main/java/com/tc/support/evict/CacheEvictContext.java
@@ -11,33 +11,59 @@ public class CacheEvictContext<K, V> implements ICacheEvictContext<K, V> {
 
     private int size;
 
+    /**
+     * 返回key
+     * @return key
+     */
     @Override
     public K key() {
         return key;
     }
 
 
-
+    /**
+     * 返回cache对象
+     * @return cache对象
+     */
     @Override
     public ICache<K, V> cache() {
         return cache;
     }
 
+    /**
+     * 返回上下文大小
+     * @return 上下文大小
+     */
     @Override
     public int size() {
         return size;
     }
 
+    /**
+     * 设置上下文的key并CacheEvictContext对象
+     * @param key
+     * @return
+     */
     public CacheEvictContext<K, V> key(K key) {
         this.key = key;
         return this;
     }
 
+    /**
+     * 设置上下文的大小并返回CacheEvictContext对象
+     * @param size
+     * @return
+     */
     public CacheEvictContext<K, V> size(int size) {
         this.size = size;
         return this;
     }
 
+    /**
+     * 设置cache并返回CacheEvictContext对象
+     * @param cache
+     * @return
+     */
     public CacheEvictContext<K, V> cache(ICache<K, V> cache) {
         this.cache = cache;
         return this;
